182c594b43a679979c9280c3c3a7f3cfae9cd35c
TheWaffleNerd <68878721+TheWaffleNerd@users.noreply.github.com>
2024-07-23 14:09:12 -0700

    Debug(2) machinelearning.py

--- machinelearning.py
+++ machinelearning.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
-from sklearn.decomposition import train_test_split
+from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from sklean.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
