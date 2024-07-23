fa006f4f4b3cc65e3a101be1b8cff67a48a6eb58
TheWaffleNerd <68878721+TheWaffleNerd@users.noreply.github.com>
2024-07-23 14:08:20 -0700

    Debug(1) machinelearning.py

--- machinelearning.py
+++ machinelearning.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn.model)selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 from sklearn.decomposition import train_test_split
 from sklearn.svm import SVC
