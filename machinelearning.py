f346ecb68d6371305d3477c8bfecf77a31f17dd1
TheWaffleNerd <68878721+TheWaffleNerd@users.noreply.github.com>
2024-07-23 14:09:43 -0700

    Debug(3) machinelearning.py

--- machinelearning.py
+++ machinelearning.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-from sklean.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.metrics import accuracy_score
