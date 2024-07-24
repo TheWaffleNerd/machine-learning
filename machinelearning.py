e2b1d0f707fba9cafe9cee3755b1492a4f2de57f
TheWaffleNerd <68878721+TheWaffleNerd@users.noreply.github.com>
2024-07-24 10:07:01 -0700

    Update machinelearning.py

--- machinelearning.py
+++ machinelearning.py
@@ -109,5 +109,5 @@ if dataset_name == 'Iris':
 elif dataset_name == 'Wine':
   data = datasets.load_wine(as_frame=True)
 else:
-    data = datasets.load_breast_cancer(as_frame=True)
+  data = datasets.load_breast_cancer(as_frame=True)
 st.dataframe(X)
